25ae931cee36606fa0cfc16c0262cf476f5f1020
Rodrigo Exterkoetter <rodrigo.exterkoetter@hexagonagriculture.com>
2016-08-29 18:39:04 -0300

    Make classifier adaline

--- adaline/adaline.py
+++ adaline/adaline.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
+from matplotlib.colors import ListedColormap
+import utils.utils
 
 df = pd.read_csv('../data_sets/iris.data', header=None)
 df.tail()
@@ -50,6 +51,23 @@ def main():
     ax[1].set_xlabel('Epochs')
     ax[1].set_ylabel('Sum-squared-error')
     ax[1].set_title('Adaline - Learning rate 0.0001')
+    plt.show()
+
+    X_std = np.copy(X)
+    X_std[:,0] = (X[:,0] - X[:,0].mean()) / X[:,0].std()
+    X_std[:,1] = (X[:,1] - X[:,1].mean()) / X[:,1].std()
+
+    ada = AdalineGD(n_iter=15, eta=0.01)
+    ada.fit(X_std, y)
+    utils.utils.plot_decision_regions(X_std, y, classifier=ada)
+    plt.title('Adaline - Gradient Descent')
+    plt.xlabel('sepal length [standardized]')
+    plt.ylabel('petal length [standardized]')
+    plt.legend(loc='upper left')
+    plt.show()
+    plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
+    plt.xlabel('Epochs')
+    plt.ylabel('Sum-squared-error')
     plt.show()
     print("The End.")
 
