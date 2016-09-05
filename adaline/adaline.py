17231f6db965577e0f6b9f085a4432d255d5deb8
Rodrigo Exterkoetter <rodrigo.exterkoetter@hexagonagriculture.com>
2016-09-05 18:16:39 -0300

    Stochastic gradient descent

--- adaline/adaline.py
+++ adaline/adaline.py
@@ -39,6 +39,38 @@ class AdalineGD(object) :
         """Return class label after unit step"""
         return np.where(self.activation(X) >= 0.0, 1, -1)
 
+class AdalineSGD(object):
+    def __init__(self, eta=0.01, n_iter=10, shuffle=True, random_state=None):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.w_initialized = False
+        self.shuffle = shuffle
+        if random_state:
+            seed(random_state)
+    
+    def fit(self, X, y):
+        self._initialize_weights(X.shape[1])
+        self.cost_ = []
+        for i in range(self.n_iter):
+            if self.shuffle:
+                X, y = self._shuffle(X, y)
+            cost = []
+            for xi, target in zip(X, y):
+                cost.append(self._update_weights(xi, target))
+            avg_cost = sum(cost)/len(y)
+            self.cost_.append(avg_cost)
+        return self
+    
+    def partial_fit(self, X, y):
+        if not self.w_initialized:
+            self._initialize_weights(X.shape[1])
+        if y.ravel().shape[0] > 1:
+            for xi, target in zip(X, y):
+                self._update_weights(xi, target)
+        else:
+            self._update_weights(X, y)
+        return self
+
 def main():
     y = df.iloc[0:100, 4].values
     y = np.where(y == 'Iris-setosa', -1, 1)
