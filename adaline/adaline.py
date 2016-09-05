a3385fb3498f4b55b1d3ad1a4420f18905f2681d
Rodrigo Exterkoetter <rodrigo.exterkoetter@hexagonagriculture.com>
2016-09-05 18:30:36 -0300

    Add stochastic GD to adaliene

--- adaline/adaline.py
+++ adaline/adaline.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.random import seed
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
@@ -71,41 +72,84 @@ class AdalineSGD(object):
             self._update_weights(X, y)
         return self
 
+    def _shuffle(self, X, y):
+        r = np.random.permutation(len(y))
+        return X[r], y[r]
+    
+    def _initialize_weights(self, m):
+        self.w_ = np.zeros(1 + m)
+        self.w_initialized = True
+
+    def _update_weights(self, xi, target):
+        output = self.net_input(xi)
+        error = (target - output)
+        self.w_[1:] += self.eta * xi.dot(error)
+        self.w_[0] += self.eta * error
+        cost = 0.5 * error**2
+        return cost
+    
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def activation(self, X):
+        return self.net_input(X)
+
+    def predict(self, X):
+        return np.where(self.activation(X) >= 0.0, 1, -1)
+
 def main():
+    
     y = df.iloc[0:100, 4].values
     y = np.where(y == 'Iris-setosa', -1, 1)
     X = df.iloc[0:100, [0, 2]].values
-    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
-    ada1 = AdalineGD(n_iter=10, eta=0.01).fit(X, y)
-    ax[0].plot(range(1, len(ada1.cost_) + 1), np.log10(ada1.cost_), marker='o')
-    ax[0].set_xlabel('Epochs')
-    ax[0].set_ylabel('log(Sum-squared-error)')
-    ax[0].set_title('Adaline - Learning rate 0.01')
-    ada2 = AdalineGD(n_iter=10, eta=0.0001).fit(X, y)
-    ax[1].plot(range(1, len(ada2.cost_) + 1), ada2.cost_, marker='o')
-    ax[1].set_xlabel('Epochs')
-    ax[1].set_ylabel('Sum-squared-error')
-    ax[1].set_title('Adaline - Learning rate 0.0001')
-    plt.show(block=False)
-    plt.figure()
-    
+
     X_std = np.copy(X)
     X_std[:,0] = (X[:,0] - X[:,0].mean()) / X[:,0].std()
     X_std[:,1] = (X[:,1] - X[:,1].mean()) / X[:,1].std()
-
-    ada = AdalineGD(n_iter=15, eta=0.01)
-    ada.fit(X_std, y)
-    plot_decision_regions(X_std, y, classifier=ada)
-    plt.title('Adaline - Gradient Descent')
-    plt.xlabel('sepal length [standardized]')
-    plt.ylabel('petal length [standardized]')
-    plt.legend(loc='upper left')
-    plt.show(block=False)
-    plt.figure()
-    plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
-    plt.xlabel('Epochs')
-    plt.ylabel('Sum-squared-error')
-    plt.show()
+    
+    stochastic = False
+    if stochastic:
+        ada = AdalineSGD(n_iter=15, eta=0.01, random_state=1)
+        ada.fit(X_std, y)
+        plot_decision_regions(X_std, y, classifier=ada)
+        plt.title('Adaline - Stochastic Gradient Descent')
+        plt.xlabel('sepal length [standardized]')
+        plt.ylabel('petal length [standardized]')
+        plt.legend(loc='upper left')
+        plt.show(block=False)
+        plt.figure()
+        plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
+        plt.xlabel('Epochs')
+        plt.ylabel('Sum-squared-error')
+        plt.show()
+    else:
+        fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
+        ada1 = AdalineGD(n_iter=10, eta=0.01).fit(X, y)
+        ax[0].plot(range(1, len(ada1.cost_) + 1), np.log10(ada1.cost_), marker='o')
+        ax[0].set_xlabel('Epochs')
+        ax[0].set_ylabel('log(Sum-squared-error)')
+        ax[0].set_title('Adaline - Learning rate 0.01')
+        ada2 = AdalineGD(n_iter=10, eta=0.0001).fit(X, y)
+        ax[1].plot(range(1, len(ada2.cost_) + 1), ada2.cost_, marker='o')
+        ax[1].set_xlabel('Epochs')
+        ax[1].set_ylabel('Sum-squared-error')
+        ax[1].set_title('Adaline - Learning rate 0.0001')
+        plt.show(block=False)
+        plt.figure()
+        
+        ada = AdalineGD(n_iter=15, eta=0.01)
+        ada.fit(X_std, y)
+        plot_decision_regions(X_std, y, classifier=ada)
+        plt.title('Adaline - Gradient Descent')
+        plt.xlabel('sepal length [standardized]')
+        plt.ylabel('petal length [standardized]')
+        plt.legend(loc='upper left')
+        plt.show(block=False)
+        plt.figure()
+        plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
+        plt.xlabel('Epochs')
+        plt.ylabel('Sum-squared-error')
+        plt.show()
     print("The End.")
 
 if __name__ == "__main__":
